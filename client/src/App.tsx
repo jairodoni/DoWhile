51b0f4dcaf086cb72c082d35fb30bfe0b3601270
jairodoni <jairo.doni97@gmail.com>
2021-10-20 20:31:30 -0300

    Design SendMessageForm component done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
 import { MessageList } from './components/MessageList';
 import { LoginBox } from './components/LoginBox';
 import styles from './styles/app.module.scss';
+import { useAuth } from './hooks/useAuth';
+import { SendMessageForm } from './components/SendMessageForm';
 
 export function App() {
+  const { user } = useAuth()
   return (
     <main className={styles.contentWrapper}>
       <MessageList />
-      <LoginBox />
+      {!!user ? <SendMessageForm /> : <LoginBox />}
     </main>
   );
 }
--- client/src/components/SendMessageForm/index.tsx
+++ client/src/components/SendMessageForm/index.tsx
@@ -1,3 +1,35 @@
+import { VscSignOut } from 'react-icons/vsc';
+import { useAuth } from '../../hooks/useAuth';
+import styles from './styles.module.scss';
+
 export function SendMessageForm() {
-  return <h1>SendMessageForm</h1>;
+  const { user, singnOut } = useAuth();
+  return (
+    <div className={styles.sendMessageFormWrapper}>
+      <button onClick={singnOut} className={styles.signOutButton}>
+        <VscSignOut size="32" />
+      </button>
+      <header className={styles.userInformation}>
+        <div className={styles.userImage}>
+          <img src={user?.avatar_url} alt={user?.name} />
+        </div>
+        <strong className={styles.userName}>{user?.name}</strong>
+        <span className={styles.userGithub}>
+          <VscSignOut size="16" />
+          {user?.login}
+        </span>
+      </header>
+
+      <form className={styles.sendMessageForm}>
+        <label htmlFor="message">Mensagem</label>
+        <textarea
+          name="message"
+          id="message"
+          placeholder="Qual sua expectativa para o evento?"
+        />
+
+        <button type="submit">Enviar mensagem</button>
+      </form>
+    </div>
+  );
 }
